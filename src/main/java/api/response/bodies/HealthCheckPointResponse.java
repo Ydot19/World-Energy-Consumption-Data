e9d3db1d92ba4664b0e570ba6b8f258f13e4d6e0
Yaadata Abdalhalim <abdalhalim.yaadata@gmail.com>
2020-08-30 09:04:16 -0400

    enhanced: added json response body to health checkpoint

---
+++ src/main/java/api/response/bodies/HealthCheckPointResponse.java
@@ -0,0 +1,9 @@
+package api.response.bodies;
+
+public class HealthCheckPointResponse {
+    public final String response;
+
+    public HealthCheckPointResponse(String i){
+        this.response = i;
+    }
+}
--- src/main/java/api/routes/HealthCheckPoint.java
+++ src/main/java/api/routes/HealthCheckPoint.java
@@ -2,14 +2,17 @@ package api.routes;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import api.response.bodies.HealthCheckPointResponse;
 
 @RestController
 public class HealthCheckPoint {
-    private static final String response = "Reached the health checkpoint\n";
+    private static final String response = "Reached the health checkpoint";
 
-    @GetMapping("/health")
-    public String getHealthResponse(){
-        return response;
+    @GetMapping(value = "/health", produces = "application/json")
+    @ResponseBody
+    public HealthCheckPointResponse getHealthResponse(){
+        return new HealthCheckPointResponse(response);
     }
 
 }
